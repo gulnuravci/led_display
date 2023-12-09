2733c42ec2615f58f9de073d390655ac3291b2d9
Rose <34399080+gulnuravci@users.noreply.github.com>
2023-12-09 14:02:41 -0500

    first commit

---
+++ animation_led.ino
@@ -0,0 +1,127 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN            6   // Define the pin where the data line is connected
+#define NUMPIXELS      522 // Number of LEDs in the strip
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  strip.begin();           // Initialize the strip
+  strip.show();            // Initialize all pixels to 'off'
+  strip.setBrightness(10); // Set brightness to 10% (max 255, so 10% of 255 is about 25)
+//  Serial.begin(9600);
+//  Serial.println("setup done...");
+}
+
+void loop() {
+  cube_animation();
+//  flower_animation();
+}
+
+void clear_LEDs(){
+  for(int i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, strip.Color(0, 0, 0)); // Set color to 'off'
+  }
+}
+
+int cube_outline[] = {148, 149, 150, 151, 152, 153, 154,
+                      177, 171, 170,
+                      184, 190, 192,
+                      213, 207, 204,
+                      220, 226, 230,
+                      249, 243, 239,
+                      256, 262, 266,
+                      285, 279, 275,
+                      292, 298, 302,
+                      321, 315, 311,
+                      328, 329, 330, 331, 332, 333, 334, 338,
+                      356, 350, 347,
+                      366, 372, 374,
+                      390, 384, 383,
+                      404, 405, 406, 407, 408, 409, 410};
+                      
+  int cube_fill[] = {176, 175, 174, 173, 172,
+                   185, 186, 187, 188, 189, 191,
+                   212, 211, 210, 209, 208, 206, 205,
+                   221, 222, 223, 224, 225, 227, 228, 229,
+                   248, 247, 246, 245, 244, 242, 241, 240,
+                   257, 258, 259, 260, 261, 263, 264, 265,
+                   284, 283, 282, 281, 280, 278, 277, 276,
+                   293, 294, 295, 296, 297, 299, 300, 301,
+                   320, 319, 318, 317, 316, 314, 313, 312,
+                   335, 336, 337,
+                   355, 354, 353, 352, 351, 349, 348,
+                   367, 368, 369, 370, 371, 373,
+                   389, 388, 387, 386, 385};
+                   
+void draw_cube(int outline_red, int outline_green, int outline_blue,
+               int fill_red, int fill_green, int fill_blue){
+  //  draw cube outline  
+  for(int i = 0; i < sizeof(cube_outline) / sizeof(cube_outline[0]); i++) {
+    int ledIndex = cube_outline[i];
+    if (ledIndex < NUMPIXELS) {
+      strip.setPixelColor(ledIndex, strip.Color(outline_red, outline_green, outline_blue));
+    }
+  }
+  
+  //  draw cube fill
+  for(int i = 0; i < sizeof(cube_fill) / sizeof(cube_fill[0]); i++) {
+    int ledIndex = cube_fill[i];
+    if (ledIndex < NUMPIXELS) {
+      strip.setPixelColor(cube_fill[i], strip.Color(fill_red, fill_green, fill_blue));
+    }
+  }
+  strip.show();
+}
+                   
+void cube_animation(){
+  clear_LEDs();
+  draw_cube(239, 0, 255, // outline - pink
+          17, 0, 255); // fill - blue
+  delay(200);
+  
+  clear_LEDs();
+  draw_cube(77, 255, 0, // outline - neon green
+          255, 137, 0); // fill - orange
+  delay(200);
+
+  clear_LEDs();
+  draw_cube(119, 0, 255, // outline - purple
+          255, 255, 0); // fill - yellow
+  delay(200);
+}
+
+int flower_orange[] = {3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 41, 42, 43, 44, 45, 46, 47, 48, 49, 61, 62, 63, 64, 65, 66, 67, 80, 81, 82};
+int flower_yellow[] = {316, 332, 333, 334, 352};
+int flower_blue[] = {241, 242, 246, 247, 257, 260, 262, 265, 276, 280, 284, 294, 297, 300, 312, 313, 314, 318, 330, 331, 335, 338, 347, 351, 356, 364, 369, 374, 383, 387, 389, 393, 400, 403, 404, 406, 407, 410, 420, 421, 422, 426, 427, 428, 439, 443, 458, 462, 475, 479, 495, 497, 513};
+int flower_green[] = {100, 117, 136, 153, 172, 189, 208, 225, 244, 261};
+
+void draw_flower(){
+  // draw orange
+  for(int i = 0; i < sizeof(flower_orange) / sizeof(flower_orange[0]); i++) {
+    strip.setPixelColor(flower_orange[i], strip.Color(255, 128, 0));
+  }
+  
+  // draw yellow
+  for(int i = 0; i < sizeof(flower_yellow) / sizeof(flower_yellow[0]); i++) {
+    strip.setPixelColor(flower_yellow[i], strip.Color(255, 255, 0));
+  }
+
+  // draw blue
+  for(int i = 0; i < sizeof(flower_blue) / sizeof(flower_blue[0]); i++) {
+    strip.setPixelColor(flower_blue[i], strip.Color(0, 0, 255));
+  }
+
+  // draw green
+  for(int i = 0; i < sizeof(flower_green) / sizeof(flower_green[0]); i++) {
+    strip.setPixelColor(flower_green[i], strip.Color(0, 255, 0));
+  }
+  
+  strip.show();
+}
+
+void flower_animation(){
+  clear_LEDs();
+  draw_flower();
+  delay(5000);
+}
---
+++ led_grid.py
@@ -0,0 +1,207 @@
+import tkinter as tk
+
+# Default color when the application starts
+current_color = "#FF0000"
+
+# Height of the color legend area at the top of the canvas
+legend_height = 50
+
+def on_canvas_click(event):
+    """
+    Handles left-click events on the canvas. This function is responsible for coloring the grid cells based on the currently selected color and updating the color lists accordingly.
+    """
+    global color_lists  # Access the global dictionary of color lists
+    if event.y > legend_height:  # Ensures click is within the grid area
+        # Calculate which column and row is clicked
+        col_width = canvas.winfo_width() / 18
+        row_height = (canvas.winfo_height() - legend_height) / 30  # Updated to 30 rows
+        row_clicked = int((event.y - legend_height) // row_height)
+        col_clicked = int(event.x // col_width)
+
+        # Adjust column for zigzag numbering if row is even
+        if row_clicked % 2 == 0:  # Adjusted condition to match even rows
+            col_clicked = 17 - col_clicked
+
+        cell_id = grid_cells[29 - row_clicked][col_clicked]  # Calculate cell id based on row and column clicked
+        canvas.itemconfig(cell_id, fill=current_color)  # Change the cell color
+        
+        # Update the color list
+        cell_number = (29 - row_clicked) * 18 + col_clicked  # Calculate linear cell number
+        remove_number_from_color_lists(cell_number)
+        color_lists[current_color].add(cell_number)
+
+def on_legend_click(event):
+    """
+    Handles left-click events on the color legend area. This function updates the current color based on the user's selection from the color legend.
+    """
+    global current_color
+    if event.y <= legend_height:  # Check if click is in the legend area
+        selected_color = int(event.x // (canvas.winfo_width() / len(colors)))
+        current_color = colors[selected_color]
+
+def on_canvas_right_click(event):
+    """
+    Handles right-click events on the canvas. This function resets the color of a grid cell to black and updates the color lists accordingly.
+    """
+    if event.y > legend_height:  # Check if click is within the grid area
+        # Calculate which column and row is clicked
+        col_width = canvas.winfo_width() / 18
+        row_height = (canvas.winfo_height() - legend_height) / 30  # Updated to 30 rows
+        row_clicked = int((event.y - legend_height) // row_height)
+        col_clicked = int(event.x // col_width)
+
+        # Adjust column for zigzag numbering if row is even
+        if row_clicked % 2 == 0:  # Adjusted condition to match even rows
+            col_clicked = 17 - col_clicked
+
+        cell_id = grid_cells[29 - row_clicked][col_clicked]  # Calculate cell id based on row and column clicked
+        canvas.itemconfig(cell_id, fill="black")  # Reset the cell color to black
+
+        # Update the color list
+        cell_number = (29 - row_clicked) * 18 + col_clicked  # Calculate linear cell number
+        remove_number_from_color_lists(cell_number)
+
+def create_grid(canvas, rows, cols):
+    """
+    Creates a grid layout on the canvas. Each cell of the grid is initialized with a black background and a white text displaying the cell number. The numbering starts from the bottom left and follows a zigzag pattern upwards.
+    """
+    canvas_width = canvas.winfo_width()
+    canvas_height = canvas.winfo_height() - legend_height
+    row_height = canvas_height / rows
+    col_width = canvas_width / cols
+
+    number = 1  # Initialize cell numbering starting with 1
+    for row_num in range(rows):
+        row = []  # Initialize the list for the current row
+        for col_num in range(cols):
+            # Determine the position based on the zigzag pattern
+            if row_num % 2 == 0:
+                # Even rows go left to right
+                col_index = col_num
+            else:
+                # Odd rows go right to left
+                col_index = (cols - 1) - col_num
+
+            # Calculate rectangle coordinates for cell
+            x0 = col_index * col_width
+            y0 = (rows - 1 - row_num) * row_height + legend_height
+            x1 = x0 + col_width
+            y1 = y0 + row_height
+            # Draw the cell on the canvas
+            rect = canvas.create_rectangle(x0, y0, x1, y1, fill="black", outline="black")
+            # Place the cell number in the center
+            canvas.create_text((x0 + x1) / 2, (y0 + y1) / 2, text=str(number), fill="white", font=("Arial", 10))
+            number += 1  # Increment cell number
+            row.append(rect)  # Add the cell to the current row
+        grid_cells.append(row)  # Add the current row to the grid cells
+
+def create_color_legend(canvas, colors):
+    """
+    Creates a color legend at the top of the canvas. This area allows users to select a color
+    for coloring the grid cells.
+    """
+    color_width = canvas.winfo_width() / len(colors)
+    for i, color in enumerate(colors):
+        x0 = i * color_width
+        x1 = x0 + color_width
+        canvas.create_rectangle(x0, 0, x1, legend_height, fill=color, outline="black")
+
+# List of colors used in the color legend
+colors = [
+    "#FF0000", "#FF7F00", "#FFFF00", "#7FFF00",
+    "#00FF00", "#00FF7F", "#00FFFF", "#007FFF",
+    "#0000FF", "#7F00FF", "#FF00FF", "#FF007F",
+    "#FFFFFF"
+]
+
+# Mapping of color hex codes to their names
+color_names = {
+    "#FF0000": "Red", "#FF7F00": "Orange", "#FFFF00": "Yellow",
+    "#7FFF00": "Chartreuse", "#00FF00": "Green", "#00FF7F": "Spring Green",
+    "#00FFFF": "Cyan", "#007FFF": "Azure", "#0000FF": "Blue",
+    "#7F00FF": "Violet", "#FF00FF": "Magenta", "#FF007F": "Rose",
+    "#FFFFFF": "White"
+}
+
+# Dictionary to store the list of cells colored with each color
+color_lists = {color: set() for color in colors}
+
+def remove_number_from_color_lists(cell_number):
+    """
+    Removes a cell number from all color lists. This function is used to ensure that a cell
+    number is only listed under its current color.
+    """
+    for color, number_set in color_lists.items():
+        number_set.discard(cell_number)
+
+def generate_color_lists():
+    """
+    Generates a string representation of the color lists. This string is used to update the
+    text widget displaying the list of cells colored with each color.
+    """
+    lists_str = ""
+    for color, grid_set in color_lists.items():
+        color_name = color_names.get(color, "Unknown")
+        sorted_numbers = sorted(grid_set)
+        lists_str += f"{color_name} List: {', '.join(map(str, sorted_numbers))}\n"
+    return lists_str[:-1]
+
+def display_color_lists():
+    """
+    Updates the text widget with the current color lists. This function is called when the
+    'Generate Lists' button is pressed.
+    """
+    lists_str = generate_color_lists()
+    text_output.delete("1.0", tk.END)
+    text_output.insert(tk.END, lists_str)
+
+def reset_grid():
+    """
+    Resets the grid to its default state. All cells are turned black, and the color lists are cleared.
+    This function is called when the 'Reset Grid' button is pressed.
+    """
+    global color_lists
+    for row in grid_cells:
+        for cell in row:
+            canvas.itemconfig(cell, fill="black")
+    color_lists = {color: set() for color in colors}
+    text_output.delete("1.0", tk.END)
+
+# Initialize the main window
+root = tk.Tk()
+root.geometry("1200x1000")
+
+# Create and pack the canvas into the window
+canvas = tk.Canvas(root)
+canvas.pack(fill="both", expand=True)
+
+# Create and pack the text widget for displaying color lists
+text_output = tk.Text(root, height=13, width=125)
+text_output.pack(pady=10)
+
+# Create a frame for buttons and pack it into the window
+button_frame = tk.Frame(root)
+button_frame.pack(side=tk.BOTTOM, pady=10)
+
+# Create and pack buttons for generating lists and resetting the grid
+generate_button = tk.Button(button_frame, text="Generate Lists", command=display_color_lists)
+generate_button.pack(side=tk.LEFT, padx=5)
+
+reset_button = tk.Button(button_frame, text="Reset Grid", command=reset_grid)
+reset_button.pack(side=tk.LEFT, padx=5)
+
+# Update the window to ensure all widgets are displayed correctly
+root.update()
+
+# Initialize the grid and color legend
+grid_cells = []
+create_grid(canvas, 30, 18)
+create_color_legend(canvas, colors)
+
+# Bind event handlers for mouse clicks on the canvas
+canvas.bind("<Button-1>", on_canvas_click)
+canvas.bind("<Button-1>", on_legend_click, add="+")
+canvas.bind("<Button-2>", on_canvas_right_click)
+
+# Start the application
+root.mainloop()
